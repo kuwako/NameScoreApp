b7b41779ef9ab92ae72fe6d4eb5b04f980c7cf3f
kuwako <kuwako@leverages.jp>
2016-02-26 19:01:08 +0900

    名前が空欄のときにバージョンごとにアラートをあげる

--- NameScoreApp/ViewController.swift
+++ NameScoreApp/ViewController.swift
@@ -20,12 +20,45 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    func showAlert() {
+        // バージョンを調べる
+        // バージョンによってアラートが違うため
+        let VERSION: Float = (UIDevice.currentDevice().systemVersion as NSString).floatValue
+        // version 8.0以上
+        if VERSION >= 8.0 {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your name", preferredStyle: .Alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .Default, handler: nil)
+            alertController.addAction(defaultAction)
+            self.presentViewController(alertController, animated: true, completion: nil)
+        } else {
+            let alert = UIAlertView()
+            alert.title = "Error"
+            alert.message = "Please enter yout name"
+            alert.addButtonWithTitle("OK")
+            alert.show()
+        }
+    }
+    
+    override func shouldPerformSegueWithIdentifier(identifier: String, sender: AnyObject?) -> Bool {
+        if identifier == "showResult" {
+            if self.textField.text == "" {
+                self.showAlert()
+                return false
+            }
+            return true
+        }
+        return true
+    }
 
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        let resultViewController:
-        ResultViewController = segue.destinationViewController as! ResultViewController
-        
-        resultViewController.myName = self.textField.text!
+        if segue.identifier == "showResult" {
+            let resultViewController:
+            ResultViewController = segue.destinationViewController as! ResultViewController
+            
+            resultViewController.myName = self.textField.text!
+            
+        }
     }
 }
 
